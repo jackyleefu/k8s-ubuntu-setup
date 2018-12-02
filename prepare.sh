c2bb29a9191a3fea4853d72161252058d2038968
fuzhongwei <fzw@fuzhongweideMacBook-Pro.local>
2018-12-02 23:52:29 +0800

    bug fix

--- prepare.sh
+++ prepare.sh
@@ -8,41 +8,41 @@ then
 fi
 
 ip=$(echo "$1" | sed 's/\./\\\./g' | sed 's/\//\\\//g')
-sed -i "s/\[\]/\[${ip}\]/g" /etc/netplan/50-cloud-init.yaml
-netplan apply
+sudo sed -i "s/\[\]/\[${ip}\]/g" /etc/netplan/50-cloud-init.yaml
+sudo netplan apply
 
 ## 禁用防火墙
-#systemctl stop firewalld
-#systemctl disable firewalld
+#sudo systemctl stop firewalld
+#sudo systemctl disable firewalld
 
 ## 禁用selinux
-#setenforce 0
-#sed -i "s///g" /etc/selinux/config
+#sudo setenforce 0
+#sudo sed -i "s///g" /etc/selinux/config
 
 ## 禁用内存交换
-swapoff -a
-sed -i "s/\/swap\.img/#\/swap\.img/g" /etc/fstab
+sudo swapoff -a
+sudo sed -i "s/\/swap\.img/#\/swap\.img/g" /etc/fstab
 
 ## 启用ip_forward
-sysctl -wq net.ipv4.ip_forward=1
-echo "net.ipv4.ip_forward = 1" >>/etc/sysctl.conf
+sudo sysctl -wq net.ipv4.ip_forward=1
+sudo su -c 'echo "net.ipv4.ip_forward = 1" >>/etc/sysctl.conf'
 
 ## 阻止DNS回路
-sed -i 's/#DNS=/DNS=114\.114\.114\.114/g' /etc/systemd/resolved.conf
-systemctl restart systemd-resolved
+sudo sed -i 's/#DNS=/DNS=114\.114\.114\.114/g' /etc/systemd/resolved.conf
+sudo systemctl restart systemd-resolved
 
 ## 安装docker
 if [[ -z `which docker` ]]
 then 
   echo "installing docker"
-  apt update
+  sudo apt update
   # step 1: 安装docker的GPG证书
-  curl -fsSL http://mirrors.aliyun.com/docker-ce/linux/ubuntu/gpg | sudo apt-key add -
+  sudo curl -fsSL http://mirrors.aliyun.com/docker-ce/linux/ubuntu/gpg | sudo apt-key add -
   # Step 2: 写入软件源信息
-  add-apt-repository "deb [arch=amd64] http://mirrors.aliyun.com/docker-ce/linux/ubuntu $(lsb_release -cs) stable"
+  sudo add-apt-repository "deb [arch=amd64] http://mirrors.aliyun.com/docker-ce/linux/ubuntu $(lsb_release -cs) stable"
   # Step 3: 更新并安装 Docker-CE
-  apt update
-  apt install -y docker-ce=18.06.1~ce~3-0~ubuntu
+  sudo apt update
+  sudo apt install -y docker-ce=18.06.1~ce~3-0~ubuntu
   echo "installed docker"
 fi
 
@@ -50,13 +50,13 @@ fi
 if [[ -z `which kubeadm` ]]
 then 
   echo "installing kubernetes tools"
-  apt update
+  sudo apt update
   # step 1: 安装docker的GPG证书
-  curl -fsSL https://mirrors.aliyun.com/kubernetes/apt/doc/apt-key.gpg | apt-key add -
+  curl -fsSL https://mirrors.aliyun.com/kubernetes/apt/doc/apt-key.gpg | sudo apt-key add -
   cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
 deb https://mirrors.aliyun.com/kubernetes/apt/ kubernetes-xenial main
 EOF
-  apt update
-  apt install -y kubelet kubeadm kubectl
+  sudo apt update
+  sudo apt install -y kubelet kubeadm kubectl
   echo "installed kubernetes tools"
 fi
