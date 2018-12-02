57e4e28a13614e2762c310798e5b167d8f10c214
fuzhongwei <fzw@fuzhongweideMacBook-Pro.local>
2018-12-02 23:56:21 +0800

    bug fix

--- prepare.sh
+++ prepare.sh
@@ -25,7 +25,7 @@ sudo sed -i "s/\/swap\.img/#\/swap\.img/g" /etc/fstab
 
 ## 启用ip_forward
 sudo sysctl -wq net.ipv4.ip_forward=1
-sudo su -c 'echo "net.ipv4.ip_forward = 1" >>/etc/sysctl.conf'
+sudo su -c "echo 'net.ipv4.ip_forward = 1' >>/etc/sysctl.conf"
 
 ## 阻止DNS回路
 sudo sed -i 's/#DNS=/DNS=114\.114\.114\.114/g' /etc/systemd/resolved.conf
@@ -37,7 +37,7 @@ then
   echo "installing docker"
   sudo apt update
   # step 1: 安装docker的GPG证书
-  sudo curl -fsSL http://mirrors.aliyun.com/docker-ce/linux/ubuntu/gpg | sudo apt-key add -
+  curl -fsSL http://mirrors.aliyun.com/docker-ce/linux/ubuntu/gpg | sudo apt-key add -
   # Step 2: 写入软件源信息
   sudo add-apt-repository "deb [arch=amd64] http://mirrors.aliyun.com/docker-ce/linux/ubuntu $(lsb_release -cs) stable"
   # Step 3: 更新并安装 Docker-CE
@@ -53,9 +53,9 @@ then
   sudo apt update
   # step 1: 安装docker的GPG证书
   curl -fsSL https://mirrors.aliyun.com/kubernetes/apt/doc/apt-key.gpg | sudo apt-key add -
-  cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
-deb https://mirrors.aliyun.com/kubernetes/apt/ kubernetes-xenial main
-EOF
+  sudo su -c "cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+    deb https://mirrors.aliyun.com/kubernetes/apt/ kubernetes-xenial main
+    EOF"
   sudo apt update
   sudo apt install -y kubelet kubeadm kubectl
   echo "installed kubernetes tools"
