af34e2acd55243eb5d8117106266105502a3507a
fuzhongwei <fzw@fuzhongweideMacBook-Pro.local>
2018-12-03 09:59:23 +0800

    bug fix

--- master.sh
+++ master.sh
@@ -1,26 +1,15 @@
 ## docker 提前从gcr的国内仓库里拉取kubernetes的核心image, 并修改为原image的tag
 echo "################## installing kubernetes images"
-curl -fsSL -O https://raw.githubusercontent.com/jackyleefu/k8s-ubuntu-setup/master/k8s-master-images
-file="k8s-master-images"
-
-if [ -f "$file" ]
-then
-  echo "$file found."
-
-  cat "$file" | while IFS='=' read -r key value
-  do
-    #echo "${key}=${value}"
-    if [[ -z $(sudo docker images | grep ${key%:*}) ]]
-    then
-      sudo docker pull ${value}
-      sudo docker tag ${value} ${key}
-      sudo docker rmi ${value}
-    fi
-  done
-
-else
-  echo "$file not found."
-fi
+curl -fsSL https://raw.githubusercontent.com/jackyleefu/k8s-ubuntu-setup/master/k8s-master-images | while IFS='=' read -r key value
+do
+  #echo "${key}=${value}"
+  if [[ -z $(sudo docker images | grep ${key%:*}) ]]
+  then
+    sudo docker pull ${value}
+    sudo docker tag ${value} ${key}
+    sudo docker rmi ${value}
+  fi
+done
 echo "installed kubernetes images ##################"
 
 # master 初始化
